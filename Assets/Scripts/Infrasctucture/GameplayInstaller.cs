b82067f957aa9cd84b68518f1e0bfef82431a6a3
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-06-14 11:50:20 +0300

    Create ProjectContext. Implement Gameplaye Installer

---
+++ Assets/Scripts/Infrasctucture/GameplayInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace Assets.Scripts.Infrasctucture
+{
+    public class GameplayInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<IAssetsProvider>().To<AssetsProvider>().AsSingle();
+            Container.Bind<ITankFactory>().To<TankFactory>().AsSingle();
+        }
+    }
+}
