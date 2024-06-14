d34525bc7d7041aa9599eefa145c234988e558a1
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-06-14 23:07:35 +0300

    Implement Teams

---
+++ Assets/Scripts/Teams/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Teams
+{
+    public class Team
+    {
+        public IEnumerable<Tank> Tanks => _tanks;
+        public Color TeamColor { get;  }
+
+        private readonly List<Tank> _tanks;
+
+        public Team(Color teamColor)
+        {
+            TeamColor = teamColor;
+            _tanks = new List<Tank>();
+        }
+
+        public void AddTank(Tank tank)
+        {
+            _tanks.Add(tank);
+        }
+    }
+}
