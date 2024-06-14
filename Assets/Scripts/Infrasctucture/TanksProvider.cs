77ad8c623d4f0c39822d7d7b687b69bb5fb7ecf3
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-06-14 13:23:01 +0300

    Implement TanksProvider

---
+++ Assets/Scripts/Infrasctucture/TanksProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Infrasctucture
+{
+    public class TanksProvider : ITanksProvider
+    {
+        public IEnumerable<Tank> Tanks => _tanks;
+
+        private readonly List<Tank> _tanks = new List<Tank>();  
+
+        public void AddTank(Tank tank)
+        {
+            _tanks.Add(tank);
+        }
+    }
+}
