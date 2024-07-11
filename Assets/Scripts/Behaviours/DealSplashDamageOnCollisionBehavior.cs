c9ac7c900c2db8780d1ac6fb9cf3a7afa69a5169
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-07-11 14:57:11 +0300

    Refactor ShellExplosion to ExplodeOnCollisionBehavior and DealSplashDamageOnCollisionBehavior

---
+++ Assets/Scripts/Behaviours/DealSplashDamageOnCollisionBehavior.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class DealSplashDamageOnCollisionBehavior : MonoBehaviour
+{
+    public LayerMask m_TankMask;                        
+    public float m_MaxDamage = 100f;                   
+    public float m_ExplosionForce = 1000f; 
+    public float m_ExplosionRadius = 5f; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            if (!targetRigidbody)
+                continue;
+
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+            IDamagable damagable = targetRigidbody.GetComponent<IDamagable>();
+
+            if (damagable == null)
+                continue;
+
+            float damage = CalculateDamage(targetRigidbody.position);
+            damagable.TakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        float explosionDistance = explosionToTarget.magnitude;
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+        float damage = relativeDistance * m_MaxDamage;
+        damage = Mathf.Max(0f, damage);
+
+        return damage;
+    }
+}
---
+++ Assets/Scripts/Behaviours/ExplodeOnCollisionBehavior.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ExplodeOnCollisionBehavior : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem m_ExplosionParticles;        
+    [SerializeField] private AudioSource m_ExplosionAudio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayExplosion();
+    }
+
+    private void PlayExplosion()
+    {
+        m_ExplosionParticles.transform.parent = null;
+
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);
+    }
+}
