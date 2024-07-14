73ea4f5f6be6076dfaaa13e25414106aaa8abc64
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-07-14 17:01:43 +0300

    Refactor move audio logic

--- Assets/Scripts/Behaviours/TankBehaviours/TankMoveControllerBehaviour.cs
+++ Assets/Scripts/Behaviours/TankBehaviours/TankMoveControllerBehaviour.cs
@@ -7,15 +7,11 @@ namespace Assets.Scripts.Behaviours.TankBehaviours
     {       
         public float m_Speed = 12f;            
         public float m_TurnSpeed = 180f;       
-        public AudioSource m_MovementAudio;    
-        public AudioClip m_EngineIdling;       
-        public AudioClip m_EngineDriving;      
-        public float m_PitchRange = 0.2f;
-      
+
         private Rigidbody m_Rigidbody;         
-        private float m_MovementInputValue;    
-        private float m_TurnInputValue;        
-        private float m_OriginalPitch;
+        public float m_MovementInputValue;    
+        public float m_TurnInputValue;        
+
 
         private IInputSource _inputSource;
 
@@ -36,11 +32,6 @@ namespace Assets.Scripts.Behaviours.TankBehaviours
             m_Rigidbody.isKinematic = true;
         }
 
-        private void Start()
-        {
-            m_OriginalPitch = m_MovementAudio.pitch;
-        }
-    
         private void Update()
         {
             m_MovementInputValue = 0;
@@ -51,8 +42,6 @@ namespace Assets.Scripts.Behaviours.TankBehaviours
                 m_MovementInputValue = _inputSource.MovementInputValue;
                 m_TurnInputValue = _inputSource.TurnInputValue;
             }
-        
-            EngineAudio();
         }
 
         private void FixedUpdate()
@@ -65,33 +54,6 @@ namespace Assets.Scripts.Behaviours.TankBehaviours
         public void SetInputSource(IInputSource inputSource)
         {
             _inputSource = inputSource;
-        }
-
-        private void EngineAudio()
-        {
-            // If there is no input (the tank is stationary)...
-            if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
-            {
-                // ... and if the audio source is currently playing the driving clip...
-                if (m_MovementAudio.clip == m_EngineDriving)
-                {
-                    // ... change the clip to idling and play it.
-                    m_MovementAudio.clip = m_EngineIdling;
-                    m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                    m_MovementAudio.Play ();
-                }
-            }
-            else
-            {
-                // Otherwise if the tank is moving and if the idling clip is currently playing...
-                if (m_MovementAudio.clip == m_EngineIdling)
-                {
-                    // ... change the clip to driving and play.
-                    m_MovementAudio.clip = m_EngineDriving;
-                    m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                    m_MovementAudio.Play();
-                }
-            }
         }
 
         private void Move()
