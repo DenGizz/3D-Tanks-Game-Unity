833ce56f574e1a7af7633b2931b26aef3103a2e5
DenGizz <69523347+DenGizz@users.noreply.github.com>
2024-07-15 16:56:17 +0300

    Refactor tank shooting

--- Assets/Scripts/Behaviours/TankBehaviours/TankShootingControlelrBehaviour.cs
+++ Assets/Scripts/Behaviours/TankBehaviours/TankShootingControlelrBehaviour.cs
@@ -23,14 +23,11 @@ namespace Assets.Scripts.Behaviours.TankBehaviours
         [SerializeField] private float _maxLaunchForce = 30f; 
         [SerializeField] private float _maxChargeTime = 0.75f;
 
-        private float _currentLaunchForce;         // The force that will be given to the shell when the fire button is released.
-        private float _chargeSpeed;                // How fast the launch force increases, based on the max charge time.
-        private bool _isFired;                       // Whether or not the shell has been launched with this button press.
+        private float _currentLaunchForce;
+        private float _chargeSpeed;
 
         private IShellFactory _shellFactory;
         private IInputSource _inputSource;
-
-
 
         [Inject]
         public void Construct(IShellFactory shellFactory)
@@ -45,39 +42,25 @@ namespace Assets.Scripts.Behaviours.TankBehaviours
 
         private void Start ()
         {
-            // The rate that the launch force charges up is the range of possible forces by the max charge time.
             _chargeSpeed = (_maxLaunchForce - _minLaunchForce) / _maxChargeTime;
         }
 
-        private void Update ()
+        private void Update()
         {
-            // If the max force has been exceeded and the shell hasn't yet been launched...
-            if (_currentLaunchForce >= _maxLaunchForce && !_isFired)
+            if (_inputSource.GetFireButtonDown)
             {
-                // ... use the max force and launch the shell.
-                _currentLaunchForce = _maxLaunchForce;
-                Fire ();
-            }
-            // Otherwise, if the fire button has just started being pressed...
-            else if (_inputSource.GetFireButtonDown)
-            {
-                // ... reset the fired flag and reset the launch force.
-                _isFired = false;
                 _currentLaunchForce = _minLaunchForce;
-
                 OnStartCharge?.Invoke();
             }
-            // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
-            else if (_inputSource.GetFireButton && !_isFired)
-            {
-                // Increment the launch force and update the slider.
+
+            if (_inputSource.GetFireButton)
                 _currentLaunchForce += _chargeSpeed * Time.deltaTime;
-            }
-            // Otherwise, if the fire button is released and the shell hasn't been launched yet...
-            else if (_inputSource.GetFireButtonUp && !_isFired)
+
+            if (_inputSource.GetFireButtonUp)
             {
-                // ... launch the shell.
-                Fire ();
+                _currentLaunchForce = Mathf.Clamp(_currentLaunchForce, _minLaunchForce, _maxLaunchForce);
+                Fire();
+                _currentLaunchForce = 0;
             }
         }
 
@@ -86,16 +69,10 @@ namespace Assets.Scripts.Behaviours.TankBehaviours
             _inputSource = inputSource;
         }
 
-
         private void Fire ()
         {
-            _isFired = true;
-
             IShell shell = _shellFactory.CreateShell(_fireTransform.position, _fireTransform.rotation);
             shell.Lunch(_fireTransform.forward, _currentLaunchForce);
-
-            _currentLaunchForce = _minLaunchForce;
-
             OnFiered?.Invoke();
         }
     }
